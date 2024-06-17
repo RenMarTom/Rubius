ddd5f4383f0a363aba06bec7ac3b2c5215e312aa
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2024-06-17 17:15:25 +0700

    Update 17.06.py

--- 17.06.py
+++ 17.06.py
@@ -1,13 +1,15 @@
 d = {}
-lst = ["+79610","+79614","+37843","+54387","+3987", "+87451"]
+lst = ["+79610", "+79614", "+37843", "+54387", "+3987", "+87451"]
 b = []
 a1 = str
 c = 0
 for i in range(len(lst)):
     a = lst[i][0:2]
-    for j in range (len(lst)):
+    for j in range(len(lst)):
         if a1 != a:
             if lst[j][0:2] == a:
-                d[a] = lst[j]
+                b.append(lst[j])
+                d[a] = b
+    b = []
     a1 = a
 print(d)
