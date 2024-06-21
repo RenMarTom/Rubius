775568fc4de29cb1aeac1767fd4434d9d5a0d8e9
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2024-06-21 18:05:04 +0700

    Update HW#7.py

--- HW#7.py
+++ HW#7.py
@@ -1,6 +1,6 @@
 a = 'Python 3.9.11 - best language!'
 b = []
 for i in range(len(a)):
-    if a[i].isdigit() == 1:
+    if a[i].isdigit() == 1 and a[i] not in [a[i+1],a[i-1]]:
        b.append(a[i])
-print(set(b))
+print(b)
