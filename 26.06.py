f76717f91ed0d44cc9eafdfe465228cac4b9a3e3
RenMarTom <105501877+RenMarTom@users.noreply.github.com>
2024-06-26 12:49:32 +0700

    Create 26.06.py

---
+++ 26.06.py
@@ -0,0 +1,11 @@
+from random import randint,choice
+names = ['Roman','Mark','Yaroslav','Sofia']
+marks = {}
+for i in range(50):
+    a = choice(names)
+    if a in marks:
+        marks[a].append(randint(2,5))
+    else:
+        marks[a] = []
+        marks[a].append(randint(2,5))
+print(marks)
